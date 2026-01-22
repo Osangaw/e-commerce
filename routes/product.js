f9b52105e8b343a1200e42b5a8f82540ec37d38a
Osangaw <wisdomosanga@gmail.com>
2026-01-22 11:21:52 +0100

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { createProduct, searchProduct, allProducts, getProductById, deleteProduct, updateProduct, deleteAllProducts } = require("../controllers/product");
 const multer = require("multer");
-const Product = require("../models/product");
 const router = express.Router();
 
 const storage = multer.diskStorage({
@@ -14,19 +13,7 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage: storage, limits: { fileSize: 5 * 1024 * 1024 } }) 
-router.get("/fix-search-index", async (req, res) => {
-  try {
-    // 1. Drop existing indexes (to clear conflicts)
-    await Product.collection.dropIndexes();
-    
-    // 2. Force create the new Text Index
-    await Product.collection.createIndex({ name: 'text', description: 'text' }, { weights: { name: 10, description: 5 } });
-    
-    res.send("✅ Success! Search Index has been rebuilt.");
-  } catch (error) {
-    res.status(500).send("❌ Error rebuilding index: " + error.message);
-  }
-});
+
 router.post("/add",upload.single("image"), createProduct);
 router.get("/search/:key", searchProduct);
 router.get("/all", allProducts);
