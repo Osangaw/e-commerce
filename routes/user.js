be2db858323bdf08ab21be7df73876c256d3b2eb
Osangaw <wisdomosanga@gmail.com>
2026-01-25 18:20:35 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,8 @@ const { signUp, signIn, verifyEmail } = require('../controllers/user');
 const { auth } = require('../middleware');
 const router = express.Router();
 
-router.post("/signUp", signUp);
-router.post("/signIn", signIn);
+router.post("/signup", signUp);
+router.post("/signin", signIn);
 router.post("/verifyEmail", verifyEmail);
 
 module.exports = router;
