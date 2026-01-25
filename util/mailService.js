5def4bf94816413ef7ff6054b6ed8a7af2915c3d
Osangaw <wisdomosanga@gmail.com>
2026-01-25 20:32:38 +0100

    Update mailService.js

--- util/mailService.js
+++ util/mailService.js
@@ -3,9 +3,7 @@ const nodemailer = require("nodemailer");
 
 const createTransporter = () => {
     return nodemailer.createTransport({
-        host: "smtp.gmail.com", // Explicitly use Gmail SMTP
-        port: 587,              // Standard secure port for Cloud Servers
-        secure: false,          // False for port 587
+    service: "gmail",          // False for port 587
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
