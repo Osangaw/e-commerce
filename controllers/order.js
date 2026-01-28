2d2f3e47891b7e74c3809904af55333b69df4266
Osangaw <wisdomosanga@gmail.com>
2026-01-28 08:34:58 +0100

    Update order.js

--- controllers/order.js
+++ controllers/order.js
@@ -49,10 +49,8 @@ exports.addOrder = async (req, res) => {
     });
 
     const savedOrder = await order.save();
-    const deleteResult = await Cart.deleteOne({ user: userId });
+    await Cart.deleteOne({ userId });
     
-    // 🔍 LOGGING: Check your terminal to see if this prints
-    console.log(`Cart deletion attempted for User ${userId}. Deleted count:`, deleteResult.deletedCount);
     res.status(201).json({ 
         message: "Order placed successfully", 
         order: savedOrder 
